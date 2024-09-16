eeeccce0de9940eacf981757f25e5ab2f7128790
mavericb <eric.boniardi@mail.polimi.it>
2024-09-16 15:43:36 -0600

    .

--- docker/start.sh
+++ docker/start.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-echo "pod2 started"
-
-if [[ $PUBLIC_KEY ]]
-then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
-    cd /
-    service ssh start
-fi
-
 # Funzione per il download dei pesi di ColabFold
 download_weights() {
     echo "Downloading ColabFold weights..."
